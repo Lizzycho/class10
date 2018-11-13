baa31712bc4c6432e6a56fdc420783f47ae20e0d
Lizzycho <43217354+Lizzycho@users.noreply.github.com>
2018-11-13 18:52:58 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,19 @@
+//var todayDate = new Date();
+
+//var todayDay = todayDate.getDate();
+//var todayMonth = todayDate.getMonth() + 1;
+//var todayYear = todayDate.getFullYear();
+
+//var formatedCode = todayDate.getDate() + "/" + todayMonth + "/" + todayDate.getFullYear()
+
+//var stringVar = "Type a bunch of words" ;
+//var stringNum = "8" ;
+//var output = Number(stringNum) + 2 ;
+//console.log(myAlert);
+//var myAlert = alert("Hey, be careful!");
+
+//var myConfirm = confirm("Do you like me?");
+
+//console.log(myPrompt);
+//var myPrompt = prompt("Enter your name:");
+//document.getElementById("para").innerHTML = myAlert ;
